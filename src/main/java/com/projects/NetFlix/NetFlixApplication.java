304aa63a8df4ba22b1927998d5ae17ce621af9c3
Pratik <chandkavathepratik@gmail.com>
2025-01-26 20:29:21 +0530

    NetFlix Clone

---
+++ src/main/java/com/projects/NetFlix/NetFlixApplication.java
@@ -0,0 +1,13 @@
+package com.projects.NetFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/NetFlix/NetFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.NetFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
